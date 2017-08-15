ddaeb71a224a1244f4100efbe6c7c1fc343842ca
Dave Glick <dave@daveaglick.com>
2017-08-15 15:02:04 -0400

    Added support for nested property/method names to ExpressionHelper

--- src/FluentFrontend/ExpressionHelper.cs
+++ src/FluentFrontend/ExpressionHelper.cs
@@ -18,15 +18,9 @@ namespace FluentFrontend
         /// </summary>
         /// <typeparam name="T">The type of instance.</typeparam>
         /// <param name="expression">The expression.</param>
+        /// <param name="nested">If <c>true</c> then the full nested property name and all parent properties will be returned.</param>
         /// <returns>The name of the member.</returns>
-        public static string GetMemberName<T>(Expression<Func<T, object>> expression)
-        {
-            if (expression == null)
-            {
-                throw new ArgumentNullException(nameof(expression));
-            }
-            return GetMemberName(expression.Body);
-        }
+        public static string GetMemberName<T>(Expression<Func<T, object>> expression, bool nested = false) => GetMemberName(expression?.Body, nested);
 
         /// <summary>
         /// Gets the member name in a simple expression.
@@ -34,51 +28,67 @@ namespace FluentFrontend
         /// <typeparam name="T">The type of instance.</typeparam>
         /// <typeparam name="TProperty">The type of property.</typeparam>
         /// <param name="expression">The expression.</param>
+        /// <param name="nested">If <c>true</c> then the full nested property name and all parent properties will be returned.</param>
         /// <returns>The name of the member.</returns>
-        public static string GetMemberName<T, TProperty>(Expression<Func<T, TProperty>> expression)
-        {
-            if (expression == null)
-            {
-                throw new ArgumentNullException(nameof(expression));
-            }
-            return GetMemberName(expression.Body);
-        }
+        public static string GetMemberName<T, TProperty>(Expression<Func<T, TProperty>> expression, bool nested = false) => GetMemberName(expression?.Body, nested);
 
         /// <summary>
         /// Gets the member name in a simple expression.
         /// </summary>
         /// <typeparam name="T">The type of instance.</typeparam>
         /// <param name="expression">The expression.</param>
+        /// <param name="nested">If <c>true</c> then the full nested property name and all parent properties will be returned.</param>
         /// <returns>The name of the member.</returns>
-        public static string GetMemberName<T>(Expression<Action<T>> expression)
-        {
-            if (expression == null)
-            {
-                throw new ArgumentNullException(nameof(expression));
-            }
-            return GetMemberName(expression.Body);
-        }
+        public static string GetMemberName<T>(Expression<Action<T>> expression, bool nested = false) => GetMemberName(expression?.Body, nested);
 
-        private static string GetMemberName(Expression expression)
+        private static string GetMemberName(Expression expression, bool nested)
         {
             if (expression == null)
             {
                 throw new ArgumentNullException(nameof(expression));
             }
 
-            switch (expression)
+            if (nested && 
+                (expression.NodeType == ExpressionType.Convert
+                || expression.NodeType == ExpressionType.ConvertChecked))
             {
-                case MemberExpression memberExpression:
-                    return memberExpression.Member.Name;
-                case MethodCallExpression methodCallExpression:
-                    return methodCallExpression.Method.Name;
-                case UnaryExpression unaryExpression:
-                    return unaryExpression.Operand is MethodCallExpression methodExpression
-                        ? methodExpression.Method.Name
-                        : ((MemberExpression)unaryExpression.Operand).Member.Name;
+                expression = (expression as UnaryExpression)?.Operand;
             }
 
-            throw new ArgumentException("Invalid expression");
+            Stack<string> names = new Stack<string>();
+            while (expression != null)
+            {
+                switch (expression)
+                {
+                    case MemberExpression memberExpression:
+                        names.Push(memberExpression.Member.Name);
+                        expression = memberExpression.Expression as MemberExpression;
+                        break;
+                    case MethodCallExpression methodCallExpression:
+                        names.Push(methodCallExpression.Method.Name);
+                        expression = methodCallExpression.Object;
+                        break;
+                    case UnaryExpression unaryExpression:
+                        names.Push(unaryExpression.Operand is MethodCallExpression methodExpression
+                            ? methodExpression.Method.Name
+                            : ((MemberExpression) unaryExpression.Operand).Member.Name);
+                        expression = null;
+                        break;
+                    default:
+                        expression = (expression as MemberExpression)?.Expression as MemberExpression;
+                        break;
+                }
+                if (!nested)
+                {
+                    break;
+                }
+            }
+
+            if (names.Count == 0)
+            {
+                throw new ArgumentException("Invalid expression");
+            }
+            return string.Join(".", names);
         }
     }
 }
--- tests/FluentFrontend.Tests/ExpressionHelperFixture.cs
+++ tests/FluentFrontend.Tests/ExpressionHelperFixture.cs
@@ -12,18 +12,19 @@ namespace FluentFrontend.Tests
     [Parallelizable(ParallelScope.Self | ParallelScope.Children)]
     public class ExpressionHelperFixture
     {
+        public class TestClass
+        {
+            public int ValueProp { get; set; }
+            public int ValueMethod() => 0;
+            public object ReferenceProp { get; set; }
+            public object ReferenceMethod() => null;
+            public object ParamMethod(int x, object y) => null;
+            public void VoidMethod() { }
+            public TestClass Nested { get; set; }
+        }
+
         public class GetMemberNameTests : ExpressionHelperFixture
         {
-            public class TestClass
-            {
-                public int ValueProp { get; set; }
-                public int ValueMethod() => 0;
-                public object ReferenceProp { get; set; }
-                public object ReferenceMethod() => null;
-                public object ParamMethod(int x, object y) => null;
-                public void VoidMethod() {}
-            }
-
             [Test]
             public void ReturnsMemberNameForValueType()
             {
@@ -82,6 +83,126 @@ namespace FluentFrontend.Tests
 
                 // Then
                 name.ShouldBe(nameof(TestClass.VoidMethod));
+            }
+
+            [Test]
+            public void ReturnsNestedMemberNameForValueType()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.ValueProp, true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.ValueProp)}");
+            }
+
+            [Test]
+            public void ReturnsTwoLevelNestedMemberNameForValueType()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.Nested.ValueProp, true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.Nested)}.{nameof(TestClass.ValueProp)}");
+            }
+
+            [Test]
+            public void ReturnsNestedMemberNameForReferenceType()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.ReferenceProp, true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.ReferenceProp)}");
+            }
+
+            [Test]
+            public void ReturnsTwoLevelNestedMemberNameForReferenceType()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.Nested.ReferenceProp, true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.Nested)}.{nameof(TestClass.ReferenceProp)}");
+            }
+
+            [Test]
+            public void ReturnsNestedMemberNameForValueMethod()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.ValueMethod(), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.ValueMethod)}");
+            }
+
+            [Test]
+            public void ReturnsTwoLevelNestedMemberNameForValueMethod()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.Nested.ValueMethod(), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.Nested)}.{nameof(TestClass.ValueMethod)}");
+            }
+
+            [Test]
+            public void ReturnsNestedMemberNameForRefefrenceMethod()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.ReferenceMethod(), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.ReferenceMethod)}");
+            }
+
+            [Test]
+            public void ReturnsTwoLevelNestedMemberNameForRefefrenceMethod()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.Nested.ReferenceMethod(), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.Nested)}.{nameof(TestClass.ReferenceMethod)}");
+            }
+
+            [Test]
+            public void ReturnsNestedMemberNameForMethodWithParameters()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.ParamMethod(default(int), default(object)), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.ParamMethod)}");
+            }
+
+            [Test]
+            public void ReturnsTwoLevelNestedMemberNameForMethodWithParameters()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.Nested.ParamMethod(default(int), default(object)), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.Nested)}.{nameof(TestClass.ParamMethod)}");
+            }
+
+            [Test]
+            public void ReturnsNestedMemberNameForVoidMethod()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.VoidMethod(), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.VoidMethod)}");
+            }
+
+            [Test]
+            public void ReturnsTwoLevelNestedMemberNameForVoidMethod()
+            {
+                // Given, When
+                string name = ExpressionHelper.GetMemberName<TestClass>(x => x.Nested.Nested.VoidMethod(), true);
+
+                // Then
+                name.ShouldBe($"{nameof(TestClass.Nested)}.{nameof(TestClass.Nested)}.{nameof(TestClass.VoidMethod)}");
             }
 
             [Test]
